44acbe6ba362faf63a0e4261455fd8bb545121e6
lomadia <hhun6840@naver.com>
2024-02-20 01:07:23 +0900

    [:rocket: 프로젝트 시작 :rocket:] vue 파일 추가

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+
+export default{
+  name: 'App',
+  created() {
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  width: 100%;
+  margin: auto;
+}
+body{
+  background: #f5f5f5;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from "@/view/HomeView.vue";
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home',
+  },
+  {
+    path: '/home',
+    component: HomeView,
+  },
+
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
