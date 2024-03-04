3c21d020ae6ca848f6b7ed4a0cd822f3e810f672
kimkyoungwook <uk7764@naver.com>
2024-03-04 13:21:45 +0900

    ✨: home front 구현

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,5 +1,31 @@
 <template>
-w
+  <header data-bs-theme="dark">
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+      <div class="container">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+          <strong>GOTCHA-DUDE</strong>
+        </a>
+      </div>
+    </div>
+  </header>
+
+  <main>
+
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <h1 class="fw-light">오디오 이미지 유출 관리 사이트</h1>
+          <p>
+            <a href="#" class="btn btn-primary my-2">오디오</a>
+            <a href="#" class="btn btn-secondary my-2">이미지</a>
+          </p>
+        </div>
+      </div>
+    </section>
+
+  </main>
+
+
 </template>
 
 <script>
@@ -22,7 +48,13 @@ export default {
 
 <style>
 
+@media (min-width: 768px) {
 
+}
+
+.bd-mode-toggle .dropdown-menu .active .bi {
+  display: block !important;
+}
 
 </style>
 
